9e6987dfb700f43e808bf8b777508c5944b5556e
Eric Bailey <popcorncolonel@gmail.com>
2017-01-05 18:41:28 -0500

    ok ok ok it's ndb now

--- main.py
+++ main.py
@@ -5,12 +5,19 @@ import json
 import jinja2
 import os
 import yaml
+import time
+
+from google.appengine.ext import ndb
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True
 )
+
+class Category(ndb.Model):
+    name = ndb.StringProperty()
+    topics = ndb.StringProperty(repeated=True)
 
 class TopicHandler(webapp2.RequestHandler):
     '''
@@ -19,12 +26,31 @@ class TopicHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {}
 
-        with open('categories.yaml', 'r') as f:
-            category_dict = yaml.load(f)
+        #temp_cat = Category(parent=ndb.Key('Category', 'sports'), name='sports', topics=[])
+        #temp_cat.put()
 
-        template_values['categories'] = category_dict
+        cats = Category.query().fetch()
+
+        template_values['categories'] = cats
         template = JINJA_ENVIRONMENT.get_template('admin.html')
         self.response.write(template.render(template_values))
+
+    def post(self):
+        '''
+        When u wanna add a new topic, this is the endpoint u wanna call
+        '''
+        cat = self.request.get('category')
+        topic = self.request.get('topic')
+        if not cat or not topic:
+            self.error(400)
+
+        key = ndb.Key('Category', cat)
+        cat = Category.query(ancestor=key).fetch()[0]
+        cat.topics.append(topic)
+        cat.put()
+        time.sleep(.5)
+        self.redirect('/admin')
+
 
 class ApiHandler(webapp2.RequestHandler):
     '''
@@ -34,14 +60,11 @@ class ApiHandler(webapp2.RequestHandler):
         category_list = self.request.get('categories', '')
         categories = category_list.split(',')
 
-        with open('categories.yaml', 'r') as f:
-            category_dict = yaml.load(f)
+        categories = Category.query(Category.name.IN(categories))
 
         topics = []
         for cat in categories:
-            if cat not in category_dict:
-                self.error(400)
-            topics.extend(category_dict[cat])
+            topics.extend(cat.topics)
 
         api_dict = {'topics': topics}
         self.response.write(json.dumps(api_dict, sort_keys=True))
